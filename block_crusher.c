9b1071f03c2336a5889dee025d4b590b1c446136
hachimamma <subhodisha2062@gmail.com>
2025-08-30 17:46:34 +0530

    UPDATE: added bonus balls

--- block_crusher.c
+++ block_crusher.c
@@ -1,30 +1,54 @@
 #include "game_manager.h"
 #include "block_crusher.h"
 #include <stdlib.h>
-#include <stdio.h>
+#include <math.h>
+#include <time.h>
 
 #define BASE_WIDTH 800
 #define BASE_HEIGHT 600
 #define BALL_TRAIL_LENGTH 16
+#define MAX_BALLS 16
+#define MAX_POWERUPS 10
+#define POWERUP_DROP_CHANCE 30
 
 static BlockCrusherGame game;
+
+typedef struct {
+    Vector2 position;
+    Vector2 speed;
+    bool active;
+    float radius;
+} PowerUp;
+
+static PowerUp powerups[MAX_POWERUPS];
+static Ball balls[MAX_BALLS];
+static int activeBallCount = 1;
 
 static float resize(float value, int baseDimension, int currentDimension) {
     return value * (currentDimension / (float)baseDimension);
 }
 
 void InitBlockCrusher(int screenWidth, int screenHeight) {
-    game.ball.position = (Vector2){screenWidth/2, screenHeight/2};
-    game.ball.speed = (Vector2){resize(5.0f, BASE_WIDTH, screenWidth), 
+    balls[0].position = (Vector2){screenWidth/2, screenHeight/2};
+    balls[0].speed = (Vector2){resize(5.0f, BASE_WIDTH, screenWidth), 
                                resize(5.0f, BASE_HEIGHT, screenHeight)};
-    game.ball.radius = resize(8.0f, BASE_WIDTH, screenWidth);
-    game.ball.active = true;
-    game.ball.trailIndex = 0;
+    balls[0].radius = resize(8.0f, BASE_WIDTH, screenWidth);
+    balls[0].active = true;
+    balls[0].trailIndex = 0;
     
     for (int i = 0; i < BALL_TRAIL_LENGTH; i++) {
-        game.ball.trail[i] = game.ball.position;
-        game.ball.trailAlpha[i] = 0.0f;
-        game.ball.trailSize[i] = game.ball.radius;
+        balls[0].trail[i] = balls[0].position;
+        balls[0].trailAlpha[i] = 0.0f;
+        balls[0].trailSize[i] = balls[0].radius;
+    }
+    
+    for (int i = 1; i < MAX_BALLS; i++) {
+        balls[i].active = false;
+    }
+    activeBallCount = 1;
+    
+    for (int i = 0; i < MAX_POWERUPS; i++) {
+        powerups[i].active = false;
     }
     
     float pd_width = resize(120.0f, BASE_WIDTH, screenWidth);
@@ -68,6 +92,69 @@ void InitBlockCrusher(int screenWidth, int screenHeight) {
     game.lives = 3;
     game.gameOver = false;
     game.paused = false;
+    
+    srand(time(NULL));
+}
+
+void SpawnPowerUp(float x, float y, int screenWidth, int screenHeight) {
+    for (int i = 0; i < MAX_POWERUPS; i++) {
+        if (!powerups[i].active) {
+            powerups[i].position = (Vector2){x, y};
+            powerups[i].speed = (Vector2){0, resize(3.0f, BASE_HEIGHT, screenHeight)};
+            powerups[i].radius = resize(8.0f, BASE_WIDTH, screenWidth);
+            powerups[i].active = true;
+            break;
+        }
+    }
+}
+
+void DoubleBalls(int screenWidth, int screenHeight) {
+    int targetBallCount = activeBallCount * 2;
+    if (targetBallCount > MAX_BALLS) targetBallCount = MAX_BALLS;
+    
+    Ball activeBalls[MAX_BALLS];
+    int currentActiveBalls = 0;
+    
+    for (int i = 0; i < MAX_BALLS; i++) {
+        if (balls[i].active) {
+            activeBalls[currentActiveBalls] = balls[i];
+            currentActiveBalls++;
+        }
+    }
+    
+    activeBallCount = 0;
+    for (int i = 0; i < MAX_BALLS; i++) {
+        balls[i].active = false;
+    }
+    
+    int ballsToAdd = (targetBallCount < currentActiveBalls * 2) ? targetBallCount : currentActiveBalls * 2;
+    
+    for (int i = 0; i < ballsToAdd && i < MAX_BALLS; i++) {
+        int sourceIndex = i % currentActiveBalls;
+        balls[i] = activeBalls[sourceIndex];
+        
+        if (i >= currentActiveBalls) {
+            float angle = (rand() % 120 - 60) * (3.14159f / 180.0f);
+            float cos_a = cosf(angle);
+            float sin_a = sinf(angle);
+            
+            float newSpeedX = balls[i].speed.x * cos_a - balls[i].speed.y * sin_a;
+            float newSpeedY = balls[i].speed.x * sin_a + balls[i].speed.y * cos_a;
+            
+            balls[i].speed.x = newSpeedX;
+            balls[i].speed.y = newSpeedY;
+        }
+        
+        balls[i].trailIndex = 0;
+        for (int k = 0; k < BALL_TRAIL_LENGTH; k++) {
+            balls[i].trail[k] = balls[i].position;
+            balls[i].trailAlpha[k] = 0.0f;
+            balls[i].trailSize[k] = balls[i].radius;
+        }
+        
+        balls[i].active = true;
+        activeBallCount++;
+    }
 }
 
 void UpdateBlockCrusher(int screenWidth, int screenHeight, struct GameManager* manager) {
@@ -91,40 +178,56 @@ void UpdateBlockCrusher(int screenWidth, int screenHeight, struct GameManager* m
         game.paddle.rect.x += pd_speed;
     }
     
-    UpdateBT(&game.ball);
-    
-    if (game.ball.active) {
-        game.ball.position.x += game.ball.speed.x;
-        game.ball.position.y += game.ball.speed.y;
+    for (int ballIdx = 0; ballIdx < MAX_BALLS; ballIdx++) {
+        if (!balls[ballIdx].active) continue;
         
-        if (game.ball.position.x >= screenWidth - game.ball.radius || game.ball.position.x <= game.ball.radius) {
-            game.ball.speed.x *= -1;
+        UpdateBT(&balls[ballIdx]);
+        
+        balls[ballIdx].position.x += balls[ballIdx].speed.x;
+        balls[ballIdx].position.y += balls[ballIdx].speed.y;
+        
+        if (balls[ballIdx].position.x >= screenWidth - balls[ballIdx].radius || 
+            balls[ballIdx].position.x <= balls[ballIdx].radius) {
+            balls[ballIdx].speed.x *= -1;
         }
-        if (game.ball.position.y <= game.ball.radius) {
-            game.ball.speed.y *= -1;
+        if (balls[ballIdx].position.y <= balls[ballIdx].radius) {
+            balls[ballIdx].speed.y *= -1;
         }
         
-        if (game.ball.position.y >= screenHeight + game.ball.radius) {
-            game.lives--;
-            if (game.lives <= 0) {
-                game.gameOver = true;
-            } else {
-                ResetBall(screenWidth, screenHeight);
+        if (balls[ballIdx].position.y >= screenHeight + balls[ballIdx].radius) {
+            balls[ballIdx].active = false;
+            activeBallCount--;
+            
+            if (activeBallCount <= 0) {
+                game.lives--;
+                if (game.lives <= 0) {
+                    game.gameOver = true;
+                } else {
+                    ResetBall(screenWidth, screenHeight);
+                }
             }
         }
         
-        if (CheckCollisionCircleRec(game.ball.position, game.ball.radius, game.paddle.rect)) {
-            game.ball.speed.y *= -1;
+        if (CheckCollisionCircleRec(balls[ballIdx].position, balls[ballIdx].radius, game.paddle.rect)) {
+            balls[ballIdx].speed.y *= -1;
             
-            float hitPosition = (game.ball.position.x - game.paddle.rect.x) / game.paddle.rect.width;
-            game.ball.speed.x = (hitPosition - 0.5f) * baseSpeedX * 2;
+            float hitPosition = (balls[ballIdx].position.x - game.paddle.rect.x) / game.paddle.rect.width;
+            balls[ballIdx].speed.x = (hitPosition - 0.5f) * baseSpeedX * 2;
         }
         
         for (int i = 0; i < game.blocksCount; i++) {
-            if (game.blocks[i].active && CheckCollisionCircleRec(game.ball.position, game.ball.radius, game.blocks[i].rect)) {
+            if (game.blocks[i].active && 
+                CheckCollisionCircleRec(balls[ballIdx].position, balls[ballIdx].radius, game.blocks[i].rect)) {
+                
                 game.blocks[i].active = false;
-                game.ball.speed.y *= -1;
+                balls[ballIdx].speed.y *= -1;
                 game.score += 10;
+                
+                if (rand() % 100 < POWERUP_DROP_CHANCE) {
+                    float centerX = game.blocks[i].rect.x + game.blocks[i].rect.width / 2;
+                    float centerY = game.blocks[i].rect.y + game.blocks[i].rect.height / 2;
+                    SpawnPowerUp(centerX, centerY, screenWidth, screenHeight);
+                }
                 
                 bool all_gone = true;
                 for (int j = 0; j < game.blocksCount; j++) {
@@ -135,14 +238,34 @@ void UpdateBlockCrusher(int screenWidth, int screenHeight, struct GameManager* m
                 }
                 
                 if (all_gone) {
-                    game.ball.speed.x *= 1.1f;
-                    game.ball.speed.y *= 1.1f;
+                    for (int k = 0; k < MAX_BALLS; k++) {
+                        if (balls[k].active) {
+                            balls[k].speed.x *= 1.1f;
+                            balls[k].speed.y *= 1.1f;
+                        }
+                    }
                     InitBlockCrusher(screenWidth, screenHeight);
                     game.score += 100;
                 }
                 
                 break;
             }
+        }
+    }
+    
+    for (int i = 0; i < MAX_POWERUPS; i++) {
+        if (!powerups[i].active) continue;
+        
+        powerups[i].position.y += powerups[i].speed.y;
+        
+        if (CheckCollisionCircleRec(powerups[i].position, powerups[i].radius, game.paddle.rect)) {
+            powerups[i].active = false;
+            DoubleBalls(screenWidth, screenHeight);
+            game.score += 50;
+        }
+        
+        if (powerups[i].position.y > screenHeight + powerups[i].radius) {
+            powerups[i].active = false;
         }
     }
 }
@@ -160,8 +283,20 @@ void DrawBlockCrusher(int screenWidth, int screenHeight) {
     
     DrawRectangleRounded(game.paddle.rect, 0.5f, 8, LIGHTGRAY);
     
-    if (game.ball.active) {
-        DrawBallWithTrail(&game.ball);
+    for (int i = 0; i < MAX_BALLS; i++) {
+        if (balls[i].active) {
+            DrawBallWithTrail(&balls[i]);
+        }
+    }
+    
+    for (int i = 0; i < MAX_POWERUPS; i++) {
+        if (powerups[i].active) {
+            DrawCircleV(powerups[i].position, powerups[i].radius, BLUE);
+            DrawCircleV(powerups[i].position, powerups[i].radius * 1.2f, Fade(SKYBLUE, 0.5f));
+            DrawCircle(powerups[i].position.x - powerups[i].radius/3, 
+                      powerups[i].position.y - powerups[i].radius/3, 
+                      powerups[i].radius/4, Fade(WHITE, 0.8f));
+        }
     }
     
     int fontSize = resize(20, BASE_HEIGHT, screenHeight);
@@ -174,6 +309,10 @@ void DrawBlockCrusher(int screenWidth, int screenHeight) {
     const char* livesText = TextFormat("LIVES: %d", game.lives);
     int livestxt_width = MeasureText(livesText, fontSize);
     DrawText(livesText, screenWidth - livestxt_width - uiPadding, uiPadding, fontSize, GREEN);
+    
+    const char* ballsText = TextFormat("BALLS: %d", activeBallCount);
+    int ballstxt_width = MeasureText(ballsText, fontSize);
+    DrawText(ballsText, screenWidth/2 - ballstxt_width/2, uiPadding, fontSize, YELLOW);
     
     if (game.paused) {
         DrawRectangle(0, 0, screenWidth, screenHeight, Fade(BLACK, 0.7f));
@@ -213,12 +352,12 @@ void DrawBlockCrusher(int screenWidth, int screenHeight) {
                 screenHeight/2 + resize(60, BASE_HEIGHT, screenHeight), rst_fs, WHITE);
     }
     
-    int ctrl_fs = resize(18, BASE_HEIGHT, screenHeight);
-    const char* controlsText = "CONTROLS: Arrow Keys = Move, P = Pause, R = Restart, M = Menu";
+    int ctrl_fs = resize(16, BASE_HEIGHT, screenHeight);
+    const char* controlsText = "CONTROLS: Arrow Keys = Move, P = Pause, R = Restart, M = Menu | Blue balls = Double your balls!";
     int ctrltxt_width = MeasureText(controlsText, ctrl_fs);
     
     if (ctrltxt_width > screenWidth - uiPadding * 2) {
-        ctrl_fs = resize(16, BASE_HEIGHT, screenHeight);
+        ctrl_fs = resize(14, BASE_HEIGHT, screenHeight);
         ctrltxt_width = MeasureText(controlsText, ctrl_fs);
     }
     
@@ -236,17 +375,26 @@ void CloseBlockCrusher(void) {
 }
 
 void ResetBall(int screenWidth, int screenHeight) {
-    game.ball.position = (Vector2){screenWidth/2, screenHeight/2};
-    game.ball.speed = (Vector2){resize(5.0f, BASE_WIDTH, screenWidth), 
+    balls[0].position = (Vector2){screenWidth/2, screenHeight/2};
+    balls[0].speed = (Vector2){resize(5.0f, BASE_WIDTH, screenWidth), 
                                resize(5.0f, BASE_HEIGHT, screenHeight)};
-    game.ball.active = true;
+    balls[0].active = true;
     
     for (int i = 0; i < BALL_TRAIL_LENGTH; i++) {
-        game.ball.trail[i] = game.ball.position;
-        game.ball.trailAlpha[i] = 0.0f;
-        game.ball.trailSize[i] = game.ball.radius;
+        balls[0].trail[i] = balls[0].position;
+        balls[0].trailAlpha[i] = 0.0f;
+        balls[0].trailSize[i] = balls[0].radius;
     }
-    game.ball.trailIndex = 0;
+    balls[0].trailIndex = 0;
+    
+    for (int i = 1; i < MAX_BALLS; i++) {
+        balls[i].active = false;
+    }
+    activeBallCount = 1;
+    
+    for (int i = 0; i < MAX_POWERUPS; i++) {
+        powerups[i].active = false;
+    }
 }
 
 void UpdateBT(Ball *ball) {
